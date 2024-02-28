9719ce959b969ad5105eca6ea360c1283eca2236
Chandana Mahajan <39641650+chandana105@users.noreply.github.com>
2024-02-28 17:49:19 +0530

    added proejct2 initialization with refactoring of directory

---
+++ Project01/ios/Pods/Target Support Files/Pods-AwesomeProject01-AwesomeProject01Tests/Pods-AwesomeProject01-AwesomeProject01Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomeProject01_AwesomeProject01Tests : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomeProject01_AwesomeProject01Tests
+@end
---
+++ Project01/ios/Pods/Target Support Files/Pods-AwesomeProject01/Pods-AwesomeProject01-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwesomeProject01 : NSObject
+@end
+@implementation PodsDummy_Pods_AwesomeProject01
+@end
