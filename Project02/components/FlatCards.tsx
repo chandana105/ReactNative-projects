ca78b53409e92efd7a7a0e9f550cb83700206ef5
Chandana Mahajan <39641650+chandana105@users.noreply.github.com>
2024-02-29 11:25:22 +0530

    project02/flatcards

--- Project02/components/FlatCards.tsx
+++ Project02/components/FlatCards.tsx
@@ -4,9 +4,58 @@ import React from 'react';
 export default function FlatCards() {
   return (
     <View>
-      <Text>FlatCards</Text>
+      <Text style={styles.headingText}>Flat Cards</Text>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text style={styles.content}>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text style={styles.content}>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text style={styles.content}>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text style={styles.content}>Red</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  headingText: {
+    paddingHorizontal: 14,
+    paddingVertical: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 12,
+    gap: 12,
+  },
+  card: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 5,
+  },
+  content: {
+    color: '#fff',
+  },
+  cardOne: {
+    backgroundColor: 'red',
+  },
+  cardTwo: {
+    backgroundColor: 'green',
+  },
+  cardThree: {
+    backgroundColor: 'blue',
+  },
+});
+
+// view is a div in mob
